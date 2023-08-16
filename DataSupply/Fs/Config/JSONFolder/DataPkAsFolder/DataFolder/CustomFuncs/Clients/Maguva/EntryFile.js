74eef4afaeea01fa77449c6d5fd684a189ed4e89
Satish Addanki <satishaddanki321@gmail.com>
2023-08-16 17:32:40 +0530

    satish

--- DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/Clients/Maguva/EntryFile.js
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/Clients/Maguva/EntryFile.js
@@ -15,7 +15,6 @@ let StartFunc = ({ inPurchasePK, inDataPk }) => {
     };
     LocalReturnObject.KTF = false;
     if (LocalCheckFunc.KTF === false) {
-        LocalReturnObject.KReason = LocalCheckFunc.KReason;
         return LocalReturnObject;
     };
 
@@ -26,77 +25,78 @@ let StartFunc = ({ inPurchasePK, inDataPk }) => {
     let LocalBillNumber = LocalPurchasePK.BillNumber;
     let LocalAliasName = LocalPurchasePK.AliasName;
 
-    if (("Qty" in LocalPurchasePK.InvGrid) === false) {
-        LocalReturnObject.KReason = "No Data in Inv Grid";
-        return LocalReturnObject;
-    };
+    // if (("Qty" in LocalPurchasePK.InvGrid) === false) {
+    //     LocalReturnObject.KReason = "No Data in Inv Grid";
+    //     return LocalReturnObject;
+    // };
 
     async.forEachOf(LocalPurchasePK.InvGrid, (InvGridvalue, InvGridkey) => {
-        async.times(InvGridvalue.Qty, (n) => {
-            let LocalFromCommonDataFolderPushData = CommonDataFolderPushData.StartFuncNoAsync({
-                inFolderName: "QrCodes",
-                inFileNameOnly: "Generate",
-                inItemName: "Barcodes",
-                inDataPK: localDatapk,
-                inDataToInsert: {
-                    CostPrice: InvGridvalue.UnitRate,
-                    ProductName: InvGridvalue.ItemName,
-                    ProductAliasName: "",
-                    SalePrice: InvGridvalue.MRP,
-                    PercentageValueAddition: InvGridvalue.PercentageValueAddition,
-                    UserDescription: `${LocalAliasName}-${inPurchasePK}-${InvGridkey}-${InvGridvalue.Qty}`,
-                    InventorySerial: InvGridkey,
-                    PurchasePk: inPurchasePK,
-                    SupplierName: LocalSupplierName,
-                    BillNumber: LocalBillNumber,
-                }
-            });
+        if ("Qty" in InvGridvalue) {
 
-            LocalReturnObject.KResult.push(LocalFromCommonDataFolderPushData);
-        }, err => {
-            if (err) {
-                console.log(err);
-                // return console.log(err);
-            };
-        });
+            async.times(InvGridvalue.Qty, (n) => {
+                let LocalFromCommonDataFolderPushData = CommonDataFolderPushData.StartFuncNoAsync({
+                    inFolderName: "QrCodes",
+                    inFileNameOnly: "Generate",
+                    inItemName: "Barcodes",
+                    inDataPK: localDatapk,
+                    inDataToInsert: {
+                        CostPrice: InvGridvalue.UnitRate,
+                        ProductName: InvGridvalue.ItemName,
+                        ProductAliasName: "",
+                        SalePrice: InvGridvalue.MRP,
+                        PercentageValueAddition: InvGridvalue.PercentageValueAddition,
+                        UserDescription: `${LocalAliasName}-${inPurchasePK}-${InvGridkey}-${InvGridvalue.Qty}`,
+                        InventorySerial: InvGridkey,
+                        PurchasePk: inPurchasePK,
+                        SupplierName: LocalSupplierName,
+                        BillNumber: LocalBillNumber,
+                    }
+                });
+
+                LocalReturnObject.KResult.push(LocalFromCommonDataFolderPushData);
+            }, err => {
+                if (err) {
+                    console.log(err);
+                    // return console.log(err);
+                };
+            });
+        };
 
         //  LocalReturnObject.KResult.push(key);
     }, err => {
         if (err) console.error(err.message);
-        // configs is now a map of JSON data
-        doSomethingWith(configs);
     });
     LocalReturnObject.KTF = true;
 
     return LocalReturnObject;
 };
 
-if (CommonMock.AllowMock) {
-    if (CommonMock.MockKey === 'haii') {
-        let LocalMockData = require('./EntryFile.json');
-
-        StartFunc({
-            inDataPk: CommonMock.DataPK,
-            ...LocalMockData
-        }).then(PromiseData => {
-            console.log('PromiseData : ', PromiseData);
-
-        });
-    };
-};
-
-
 // if (CommonMock.AllowMock) {
-//     if (CommonMock.MockKey === 'Hai') {
+//     if (CommonMock.MockKey === 'haiii') {
 //         let LocalMockData = require('./EntryFile.json');
 
-//         let Output = StartFunc({
+//         StartFunc({
 //             inDataPk: CommonMock.DataPK,
 //             ...LocalMockData
-//         });
-//         console.log('Output : ', Output);
+//         }).then(PromiseData => {
+//             console.log('PromiseData : ', PromiseData);
 
+//         });
 //     };
 // };
+
+
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === 'haiii') {
+        let LocalMockData = require('./EntryFile.json');
+
+        let Output = StartFunc({
+            inDataPk: CommonMock.DataPK,
+            ...LocalMockData
+        });
+        console.log('Output : ', Output);
+
+    };
+};
 
 module.exports = { StartFunc };
